c6fbda6ed9509d72fd6c9296c491a3d536088e15
wyutyihnin-gg <wyutyihnin4@gmail.com>
2021-01-08 22:18:01 +0630

    "task list finished"	modified:   dom-projects/task-list/app.js,new file:   dom-projects/task-list/readme.md

--- dom-projects/task-list/app.js
+++ dom-projects/task-list/app.js
@@ -10,14 +10,50 @@ loadEventListener();
 
 //add task event
 function loadEventListener() {
+    //DOM load event
+    document.addEventListener('DOMContentLoaded', getTasks);
     //add task 
     form.addEventListener('submit', addTask);
     //remove task
     taskList.addEventListener('click', removeTask);
     //clear task event
-    clearBtn.addEventListener('click', clearTask)
+    clearBtn.addEventListener('click', clearTask);
+    //filter task event
+    filter.addEventListener('keyup', filterTask);
 }
 
+function getTasks() {
+    let tasks;
+
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+
+    tasks.forEach(function(task) {
+        //create li element
+        const li = document.createElement('li');
+        //add class
+        li.className = "collection-item";
+        //create text node and append to li
+        li.appendChild(document.createTextNode(task));
+
+        //create link element
+        const link = document.createElement('a');
+
+        //add class
+        link.className = "delete-item secondary-content"
+        link.innerHTML = "<i class='fa fa-remove'></i>"
+
+        //add link into li
+        li.appendChild(link);
+
+        //add li into ul
+        taskList.appendChild(li)
+    })
+
+}
 //Add task
 function addTask(e) {
 
@@ -30,7 +66,7 @@ function addTask(e) {
     //add class
     li.className = "collection-item";
     //create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value))
+    li.appendChild(document.createTextNode(taskInput.value));
 
     //create link element
     const link = document.createElement('a');
@@ -45,10 +81,27 @@ function addTask(e) {
     //add li into ul
     taskList.appendChild(li)
 
+    //store in LS
+    storeTaskInLocalStorage(taskInput.value);
     //clear task input
     taskInput.value = ''
 
     e.preventDefault();
+}
+
+//store task
+function storeTaskInLocalStorage(task) {
+    let tasks;
+
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+
+    tasks.push(task)
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
 //remove Task
@@ -56,10 +109,31 @@ function removeTask(e) {
     if (e.target.parentElement.classList.contains('delete-item')) {
         if (confirm('Are you sure?')) {
             e.target.parentElement.parentElement.remove()
+
+            //remove from LS
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
 }
 
+//remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks;
+
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+
+    tasks.forEach(function(task, index) {
+        if (taskItem.textContent === task) {
+            tasks.splice(index, 1);
+        }
+
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    })
+}
 //clear task
 function clearTask(e) {
     // taskList.innerHTML = ''
@@ -67,5 +141,29 @@ function clearTask(e) {
     //faster way
     while (taskList.firstChild) {
         taskList.removeChild(taskList.firstChild)
+
+        //remove from LS
+        clearTaskFromLocalStorage();
     }
+}
+
+//clear task from LS
+function clearTaskFromLocalStorage(taskList) {
+    localStorage.clear()
+}
+
+//meaing of firstChild -> there is something in the list
+//filter task
+function filterTask(e) {
+    const text = e.target.value;
+
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block'
+        } else {
+            task.style.display = 'none'
+        }
+    })
 }
---
+++ dom-projects/task-list/readme.md
@@ -0,0 +1,2 @@
+this is dom first project ( task list)
+you can get insight about adding task, removing each task and all task and localStorage processing
